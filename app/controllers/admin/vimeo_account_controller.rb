9d180e078b571bd3cb4c96e9f4a3032aea2b9c68
Marian <marian@bitflut.com>
2011-03-19 00:23:06 +0100

    - Update authorization

--- app/controllers/admin/vimeo_account_controller.rb
+++ app/controllers/admin/vimeo_account_controller.rb
@@ -1,29 +1,54 @@
 module Admin
   class VimeoAccountController < Admin::BaseController
-
+    
+    CONSUMER_KEY_FORMAT = /[a-z0-9]{32}/
+    CONSUMER_SECRET_FORMAT = /[a-z0-9]{16}/
+    TOKEN_FORMAT = /[a-z0-9]{32}/
+    SECRET_FORMAT = /[a-z0-9]{40}/
+    
     def authorization
-      #unless current_admin.vimeo_authorized?
-      #  base = Vimeo::Advanced::Base.new(
-      #    MoxieSetting.find_or_set('vimeo_consumer_key', :value => "Consumer Key"),
-      #    MoxieSetting.find_or_set('vimeo_consumer_secret', :value => "Consumer Secret"))
-      #  request_token = base.get_request_token
-      #  session[:oauth_secret] = request_token.secret
-      #  redirect_to base.authorize_url
-      #else
-      #  vimeo_callback
-      #end
+      unless authorized?
+        base = Vimeo::Advanced::Base.new(
+          account[:vimeo_consumer_key],
+          account[:vimeo_consumer_secret])
+        request_token = base.get_request_token
+        session[:oauth_secret] = request_token.secret
+        redirect_to base.authorize_url
+      elsif ready_to_authorize?
+        vimeo_callback
+      else
+        render :text => 'Not ready to authorize. Type in consumer_key and consumer_secret.'
+      end
     end
   
     def callback
-      #base = Vimeo::Advanced::Base.new(
-      #  MoxieSetting.find_or_set('vimeo_consumer_key', :value => "Consumer Key"),
-      #  MoxieSetting.find_or_set('vimeo_consumer_secret', :value => "Consumer Secret"))
-      #access_token = base.get_access_token(params[:oauth_token], session[:oauth_secret], params[:oauth_verifier])
-      #MoxieSetting.find_or_set('vimeo_token', :value => access_token.token)
-      #MoxieSetting.find_or_set('vimeo_secret', :value => access_token.secret)
-      #flash[:notice] = "You successfully authorized your vimeo account for integration in your backend. You can now start using it."
-      #redirect_to '/admin'
+      base = Vimeo::Advanced::Base.new(
+        account[:vimeo_consumer_key],
+        account[:vimeo_consumer_secret])
+      access_token = base.get_access_token(params[:oauth_token], session[:oauth_secret], params[:oauth_verifier])
+      account[:vimeo_token].update_attribute(:value, access_token.token)
+      account[:vimeo_secret].update_attribute(:value, access_token.secret)
+      flash[:notice] = "You successfully authorized your vimeo account for integration in your backend. You can now start using it."
+      redirect_to '/admin'
     end
+    
+    private
+      
+      def ready_to_authorize?
+        return account[:vimeo_consumer_key] =~ CONSUMER_KEY_FORMAT and account[:vimeo_consumer_secret] =~ CONSUMER_SECRET_FORMAT
+      end
+    
+      def authorized?
+        return account[:vimeo_token] =~ TOKEN_FORMAT and account[:vimeo_secret] =~ SECRET_FORMAT
+      end
+      
+      def get_account
+        account = {
+          :vimeo_consumer_key => MoxieSetting.find_by_name('vimeo_consumer_key').value,
+          :vimeo_consumer_secret => MoxieSetting.find_by_name('vimeo_consumer_secret').value,
+          :vimeo_token => MoxieSetting.find_by_name('vimeo_token').value,
+          :vimeo_secret => MoxieSetting.find_by_name('vimeo_secret').value}
+      end
 
   end
 end
